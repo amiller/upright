16626be476d1970c11474ab49d0b7d1b4e6dd39e
Andrew <amiller@cs.umd.edu>
2015-10-26 20:32:50 -0700

    Updated the simple experiment to use filters

--- run-experiment.sh
+++ run-experiment.sh
@@ -6,7 +6,7 @@
 # +-----------------------------------+
 # |        |        | exec0 |         |
 # |        |        |       |         |
-# | order0 | order1 |-------+ unused  |
+# | order0 | order1 |-------+ filter  |
 # |________|________| exec1 |_________|  
 # |        |        |       |         |
 # |        |        |-------+         |
@@ -18,10 +18,10 @@
 export JAVA_OPTS="-cp conf:dist/lib/bft.jar:lib/FlexiCoreProvider-1.6p3.signed.jar:lib/CoDec-build17-jdk13.jar:lib/netty-3.1.4.GA.jar"
 
 echo java ${JAVA_OPTS} BFT.order.OrderBaseNode 0 config.properties
-tmux new-session    'sleep 2; bash -c "java ${JAVA_OPTS} BFT.order.OrderBaseNode 0 config.properties | tee order0.log"; bash' \;  \
+tmux new-session    "sleep 2; bash -c 'java ${JAVA_OPTS} BFT.order.OrderBaseNode 0 config.properties | tee order0.log'; bash" \;  \
     splitw -h -p 75 'sleep 2; java ${JAVA_OPTS} BFT.order.OrderBaseNode 1 config.properties | tee order1.log; bash' \;  \
     splitw -h -p 66 'sleep 1; java ${JAVA_OPTS} Applications.hashtable.HTServer 0 config.properties ./httest/0/log ./httest/0/ss | tee exec0.log' \; \
-    splitw -h -p 50 'bash' \; \
+    splitw -h -p 50 'java ${JAVA_OPTS} BFT.filter.FilterBaseNode 0 config.properties' \; \
     selectp -t 2 \; \
     splitw -v -p 66 'sleep 1; java ${JAVA_OPTS} Applications.hashtable.HTServer 1 config.properties ./httest/1/log ./httest/1/ss | tee exec1.log' \; \
     splitw -v -p 50 'sleep 1; java ${JAVA_OPTS} Applications.hashtable.HTServer 2 config.properties ./httest/2/log ./httest/2/ss | tee exec2.log' \; \
